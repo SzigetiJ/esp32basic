cea3b1de19343e993c3b2c20b45407d2b4b225b5
SZIGETI János <szigeti@tmit.bme.hu>
2024-08-06 09:45:01 +0200

    [ISSUE-3]: RMT blink example and source doc.

--- examples/1rmtblink/rmtblink.c
+++ examples/1rmtblink/rmtblink.c
@@ -22,7 +22,8 @@
 
 // =================== Hard constants =================
 // #1: Timings
-#define RMTBLINK_PERIOD_MS  2000U
+#define BLINKLEN_MS         200U      ///< Length of a single period (on + off part).
+#define RMTBLINK_PERIOD_MS  2000U     ///< Higher than 8 * 0.2s, so RMT blinks will not overlap.
 #define RMT_DIVISOR         80U       ///< RMT TICK freq is 80MHz / RMT_DIVISOR (80 -> 1MHz, 40 -> 2MHz, 30 -> 2.66MHz)
 
 // #2: Channels / wires / addresses
@@ -35,7 +36,8 @@
 // ============= Local types ===============
 
 // ================ Local function declarations =================
-static void _rmt_init_channel(E_RMT_CHANNEL eChannel, uint8_t u8Pin);
+static void _rmt_init_controller();
+static void _rmt_init_channel(E_RMT_CHANNEL eChannel, uint8_t u8Pin, bool bLevel, bool bHoldLevel);
 static void _rmtblink_init();
 static void _rmtblink_cycle(uint64_t u64Ticks);
 
@@ -45,72 +47,80 @@ const uint16_t gu16Tim00Divisor = TIM0_0_DIVISOR;
 const uint64_t gu64tckSchedulePeriod = (CLK_FREQ_HZ / SCHEDULE_FREQ_HZ);
 
 // ==================== Local Data ================
-// the on / off length of the blinking is defined as the product of ~Base and ~Mul.
-// ~Base cannot exceed 32767.
-// ~Mul must be less than 16.
-const uint16_t u16usBlinkLenBase = 25000U;
-const uint16_t u8BlinkLenMul = 4;
+// The on / off length of the blinking is defined as the product of ~Base and ~Mul.
+// Why? Because the on/off lengths are measured in APB clock / divisor ticks (1MHz),
+// and the chosen values (0.1s = 100000 ticks) are higher than the RMT RAM entries can hold as period values.
+// Thus multiple concatenated RMT entries will define a single on or off period.
+// ~Base cannot exceed 32767 (limit of RMT entry.period).
+// ~Mul * BlinkMax must be less than 128 (RMT RAM (by default) can contain 256 entries,
+// and we use at most 2 * BlinkMax * BlinkLenMul).
+static const uint16_t gu16usBlinkLenBase = 25000U;
+static const uint16_t gu8BlinkLenMul = (BLINKLEN_MS * 1000) / (2 * gu16usBlinkLenBase);   // BLINKLEN=200 => 4.
+static const uint8_t gu8BlinkMax = 8;
 
 // ==================== Implementation ================
 
-static void _rmt_init_channel(E_RMT_CHANNEL eChannel, uint8_t u8Pin) {
-  gpio_pin_out_on(u8Pin);
-  IomuxGpioConfReg rRmtConf = {.u1FunIE = 1, .u1FunWPU = 1, .u3McuSel = 2};
-  iomux_set_gpioconf(u8Pin, rRmtConf);
-
-  // output enable
-  gpio_pin_enable(u8Pin);
-  gpio_matrix_out(u8Pin, rmt_out_signal(eChannel), 0, 0);
-  //  gpio_matrix_in(u8Pin, rmt_in_signal(eChannel), 0);
-
+static void _rmt_init_controller() {
   dport_regs()->PERIP_CLK_EN |= 1 << DPORT_PERIP_BIT_RMT;
 
   dport_regs()->PERIP_RST_EN |= 1 << DPORT_PERIP_BIT_RMT;
   dport_regs()->PERIP_RST_EN &= ~(1 << DPORT_PERIP_BIT_RMT);
 
-  S_RMT_CHCONF0_REG rChConf0 = {.u8DivCnt = RMT_DIVISOR, .u4MemSize = 1};
-  S_RMT_CHCONF1_REG rChConf1 = {.bRefAlwaysOn = 1, .bRefCntRst = 1, .bMemRdRst = 1};
-  S_RMT_APB_CONF_REG rApbConf = {.bMemAccessEn = 1, .bMemTxWrapEn = 1};
-
-  gpsRMT->asChConf[RMTBLINK_CH].r0 = rChConf0;
-  gpsRMT->asChConf[RMTBLINK_CH].r1 = rChConf1;
+  S_RMT_APB_CONF_REG rApbConf = {.bMemAccessEn = 1, .bMemTxWrapEn = 1}; // direct RMT RAM access (not using FIFO), mem wrap-around (not used)
   gpsRMT->rApb = rApbConf;
-  gpsRMT->arTxLim[RMTBLINK_CH].u9Val = 256; // currently unused
+}
+
+static void _rmt_init_channel(E_RMT_CHANNEL eChannel, uint8_t u8Pin, bool bLevel, bool bHoldLevel) {
+  // gpio & iomux
+  bLevel ? gpio_pin_out_on(u8Pin) : gpio_pin_out_off(u8Pin); // set GPIO level when not bound to RMT (optional)
+
+  IomuxGpioConfReg rRmtConf = {.u1FunIE = 1, .u1FunWPU = 1, .u3McuSel = 2}; // input enable, pull-up, iomux function
+  iomux_set_gpioconf(u8Pin, rRmtConf);
+
+  gpio_pin_enable(u8Pin);
+  gpio_matrix_out(u8Pin, rmt_out_signal(eChannel), 0, 0);
+  gpio_matrix_in(u8Pin, rmt_in_signal(eChannel), 0);
+
+  // rmt channel config
+  S_RMT_CHCONF rChConf = {
+    .r0 =
+    {.u8DivCnt = RMT_DIVISOR, .u4MemSize = 1},
+    .r1 =
+    {.bRefAlwaysOn = 1, .bRefCntRst = 1, .bMemRdRst = 1, .bIdleOutLvl = bLevel, .bIdleOutEn = bHoldLevel}
+  };
+  gpsRMT->asChConf[eChannel] = rChConf;
+
+  gpsRMT->arTxLim[eChannel].u9Val = 256; // currently unused
 }
 
 static void _rmtblink_init() {
-  _rmt_init_channel(RMTBLINK_CH, RMTBLINK_GPIO);
+  _rmt_init_controller();
+  _rmt_init_channel(RMTBLINK_CH, RMTBLINK_GPIO, 0, 0);
 }
 
 static void _rmtblink_cycle(uint64_t u64Ticks) {
   static uint64_t u64NextTick = 0;
   static uint16_t au16BlinkPattern[RMT_AUXBUF_SIZE]; // aux. buffer
   static uint8_t u8Blink = 1;
-  const uint8_t u8BlinkMax = 8;
 
   if (u64NextTick <= u64Ticks) {
-    // reset mem(ptr)
-    S_RMT_CHCONF1_REG rConf1RdRst = {.bMemRdRst = 1};
-    S_RMT_CHCONF1_REG rConf1TxSt = {.bTxStart = 1};
-    register_set_bits((RegAddr)&(gpsRMT->asChConf[RMTBLINK_CH].r1), rConf1RdRst.raw, rConf1RdRst.raw);
-
     // set pattern
     memset(au16BlinkPattern, 0, 2 * RMT_AUXBUF_SIZE);
     for (int i = 0; i < u8Blink; ++i) {
-      for (int j = 0; j < u8BlinkLenMul; ++j) {
-        au16BlinkPattern[2 * i * u8BlinkLenMul + j] = RMT_SIGNAL1 | u16usBlinkLenBase;
-        au16BlinkPattern[(2 * i + 1) * u8BlinkLenMul + j] = RMT_SIGNAL0 | u16usBlinkLenBase;
+      for (int j = 0; j < gu8BlinkLenMul; ++j) {
+        au16BlinkPattern[2 * i * gu8BlinkLenMul + j] = RMT_SIGNAL1 | gu16usBlinkLenBase;
+        au16BlinkPattern[(2 * i + 1) * gu8BlinkLenMul + j] = RMT_SIGNAL0 | gu16usBlinkLenBase;
       }
     }
-    if (u8Blink == u8BlinkMax) {
+    if (u8Blink == gu8BlinkMax) {
       u8Blink = 0;
     }
     ++u8Blink;
-    // write pattern into rmt ram
-    memcpy((void*) rmt_ram_block(RMTBLINK_CH), au16BlinkPattern, 2 * RMT_AUXBUF_SIZE);
 
-    // start tx
-    register_set_bits((RegAddr)&(gpsRMT->asChConf[RMTBLINK_CH].r1), rConf1TxSt.raw, rConf1TxSt.raw);
+    // write pattern into rmt ram & start tx
+    memcpy((void*) rmt_ram_block(RMTBLINK_CH), au16BlinkPattern, 2 * RMT_AUXBUF_SIZE);
+    gpsRMT->arInt[RMT_INT_ENA] = rmt_int_bit(RMTBLINK_CH, RMT_INT_TXEND);
+    rmt_start_tx(RMTBLINK_CH, 1);
 
     u64NextTick += MS2TICKS(RMTBLINK_PERIOD_MS);
   }
--- src/rmt.h
+++ src/rmt.h
@@ -11,21 +11,22 @@ extern "C" {
 #define RMT_SIGNAL1 (0x8000)
 #define RMT_SIGNAL0 (0x0000)
 
-#define RMT_RAM_SIZE 64
+#define RMT_CHANNEL_NUM     8U
+#define RMT_RAM_BLOCK_SIZE  64U
 
   typedef enum {
-    RMT_INT_RAW = 0,
-    RMT_INT_ST = 1,
-    RMT_INT_ENA = 2,
-    RMT_INT_CLR = 3
-  } E_RMT_INT_REG;
+    RMT_INT_RAW = 0, ///< Raw interrupt status (RO)
+    RMT_INT_ST = 1, ///< Masked interrupt status (RO)
+    RMT_INT_ENA = 2, ///< Setting a bit enables the given interrupt (RW)
+    RMT_INT_CLR = 3 ///< Setting a bit disables the given interrupt (WO)
+  } E_RMT_INT_REG; ///< Types of interrupt registers.
 
   typedef enum {
-    RMT_INT_TXEND = 0,
-    RMT_INT_RXEND = 1,
-    RMT_INT_ERR = 2,
-    RMT_INT_TXTHRES = 3
-  } E_RMT_INT_TYPE;
+    RMT_INT_TXEND = 0, ///< TX is done.
+    RMT_INT_RXEND = 1, ///< RX is done.
+    RMT_INT_ERR = 2, ///< An error occured.
+    RMT_INT_TXTHRES = 3 ///< TX threshold is reached.
+  } E_RMT_INT_TYPE; ///< Types of RMT interrupt.
 
   typedef enum {
     RMT_CH0 = 0,
@@ -41,14 +42,14 @@ extern "C" {
   typedef volatile union {
 
     volatile struct {
-      uint32_t u8DivCnt : 8; ///< the divider for the channel clock of channel n. (R/W)
+      uint32_t u8DivCnt : 8; ///< The divisor for the channel clock of channel n. (R/W)
 
       uint32_t u16IdleThres : 16; ///< In receive mode, when no edge is detected on the input signal for longer than REG_IDLE_THRES_CHn channel clock cycles, the receive process is finished. (R/W)
 
-      uint32_t u4MemSize : 4; ///< the amount of memory blocks allocated to channel n. (R/W)
+      uint32_t u4MemSize : 4; ///< The amount of memory blocks allocated to channel n. (R/W)
       uint32_t bCarrierEn : 1; ///< Carrier modulation is enabled with 1, while carrier modulation is disabled with 0. (R/W)
-      uint32_t bCarrierOutLvl : 1; ///< when the carrier wave is being transmitted. Transmit on low output level with 0, and transmit on high output level with 1. (R/W)
-      uint32_t bMemPd : 1; ///< power down the entire RMT RAM block.Reset (It only exists in RMT_CH0CONF0). 1: power down memory; 0: power up memory. (R/W)
+      uint32_t bCarrierOutLvl : 1; ///< Used when the carrier wave is being transmitted. Transmit on low output level with 0, and transmit on high output level with 1. (R/W)
+      uint32_t bMemPd : 1; ///< Power down the entire RMT RAM block.Reset (It only exists in RMT_CH0CONF0). 1: power down memory; 0: power up memory. (R/W)
       uint32_t rsvd31 : 1;
     };
     volatile uint32_t raw;
@@ -61,7 +62,7 @@ extern "C" {
       uint32_t bRxEn : 1; ///< Set this bit to enable receiving data on channel n. (R/W)
       uint32_t bMemWrRst : 1; ///< Set this bit to reset the write-RAM address for channel n by accessing the receiver. (R/W)
       uint32_t bMemRdRst : 1; ///< Set this bit to reset the read-RAM address for channel n by accessing the transmitter. (R/W)
-      uint32_t rsvd4 : 1; ///< reserved
+      uint32_t bFifoRst : 1; ///< Undocumented bit. Probably resets FIFO internal counters (R/W).
       uint32_t bMemOwner : 1; ///< Number 1 indicates that the receiver is using the RAM, while 0 indicates that the transmitter is using the RAM. (R/W)
       uint32_t bTxContiMode : 1; ///< If this bit is set, instead of going to an idle state when transmission ends, the transmitter will restart transmission. This results in a repeating output signal. (R/W)
       uint32_t bRxFilterEn : 1; ///< receive filter’s enable-bit for channel n. (R/W)
@@ -69,9 +70,9 @@ extern "C" {
       uint32_t u8RxFilterThres : 8; ///< In receive mode, channel n ignores input pulse when the pulse width is smaller than this value in APB clock periods. (R/W)
 
       uint32_t bRefCntRst : 1; ///< Setting this bit resets the clock divider of channel n. (R/W)
-      uint32_t bRefAlwaysOn : 1; ///< select the channel’s base clock. 1:clk_apb; 0:clk_ref. (R/W)
-      uint32_t bIdleOutLvl : 1; ///< the level of output signals in channel n when the latter is in IDLE state. (R/W)
-      uint32_t bIdleOutEn : 1; ///< output enable-control bit for channel n in IDLE state. (R/W)
+      uint32_t bRefAlwaysOn : 1; ///< Select the channel’s base clock. 1:clk_apb; 0:clk_ref. (R/W)
+      uint32_t bIdleOutLvl : 1; ///< The level of output signals in channel n when the latter is in IDLE state. (R/W)
+      uint32_t bIdleOutEn : 1; ///< Output enable-control bit for channel n in IDLE state. (R/W)
       uint32_t rsvd20 : 12; ///< Reserved
     };
     volatile uint32_t raw;
@@ -83,14 +84,14 @@ extern "C" {
   } S_RMT_CHCONF;
 
   typedef struct {
-    uint32_t u16Low : 16;
-    uint32_t u16High : 16;
+    uint32_t u16Low : 16; ///< If carrier is enabled, this value defines the length of low level in the carrier wave (APB/REF ticks).
+    uint32_t u16High : 16; ///< If carrier is enabled, this value defines the length of high level in the carrier wave (APB/REF ticks).
   } S_RMT_CHCARRIER_DUTY_REG;
 
   typedef union {
 
     volatile struct {
-      uint32_t u9Val : 9;
+      uint32_t u9Val : 9; ///< After sending this amount of TX entries, TX_THR_EVENT interrupt is produced.
       uint32_t rsvd9 : 23;
     };
     volatile uint32_t raw;
@@ -99,45 +100,112 @@ extern "C" {
   typedef union {
 
     volatile struct {
-      uint32_t bMemAccessEn : 1;
-      uint32_t bMemTxWrapEn : 1;
+      uint32_t bMemAccessEn : 1; ///< Direct (1) / FIFO (0) access of RMT RAM.
+      uint32_t bMemTxWrapEn : 1; ///< Enable/disable TX wraparound.
       uint32_t rsvd2 : 30;
     };
     volatile uint32_t raw;
   } S_RMT_APB_CONF_REG;
 
+  // ---------------- undocumented register types -------------------
+
+  typedef Reg S_RMT_STATUS_REG;
+
   typedef struct {
-    Reg rsvd0[8];
-    S_RMT_CHCONF asChConf[8];
-    Reg rsvd24[16];
+    uint16_t u16TxOffset;
+    uint16_t u16RxOffset;
+  } S_RMT_FIFO_OFFSET_REG;
+
+  typedef struct {
+    Reg arFifo[8]; ///< Undocumented registers. Probably RAM access (R/W) as FIFO for each channel.
+    S_RMT_CHCONF asChConf[RMT_CHANNEL_NUM];
+    S_RMT_STATUS_REG asStatus[RMT_CHANNEL_NUM];
+    S_RMT_FIFO_OFFSET_REG asFifoOffset[RMT_CHANNEL_NUM];
     Reg arInt[4];
-    S_RMT_CHCARRIER_DUTY_REG arCarrierDuty[8];
-    S_RMT_CH_TX_LIM_REG arTxLim[8];
+    S_RMT_CHCARRIER_DUTY_REG arCarrierDuty[RMT_CHANNEL_NUM];
+    S_RMT_CH_TX_LIM_REG arTxLim[RMT_CHANNEL_NUM];
     S_RMT_APB_CONF_REG rApb;
+    Reg rsvd61;
+    Reg rsvd62;
+    Reg rVersion;
   } RMT_Type;
 
   extern RMT_Type gsRMT;
-  extern Reg grRMTRAM[8 * 64];
+  extern Reg grRMTRAM[RMT_CHANNEL_NUM * RMT_RAM_BLOCK_SIZE];
   static RMT_Type *gpsRMT = &gsRMT;
   static RegAddr gpsRMTRAM = (RegAddr) grRMTRAM;
 
-  static inline RegAddr rmt_ram_block(uint8_t i) {
-    return gpsRMTRAM + RMT_RAM_SIZE * i;
+  /**
+   * Tells the RMT RAM base address of an RMT channel.
+   * @param eChannel Identifies the RMT channel.
+   * @return Base address of the RMT RAM of the given channel.
+   */
+  static inline RegAddr rmt_ram_block(E_RMT_CHANNEL eChannel) {
+    return gpsRMTRAM + RMT_RAM_BLOCK_SIZE * eChannel;
   }
 
+  /**
+   * Tells the bit position of an interrupt in RMT interrupt registers (RAW / ST / ENA / CLR).
+   * @param eChannel Identifies the RMT channel.
+   * @param eType Type of the RMT interrupt.
+   * @return Bit position in the interrupt registers.
+   */
   static inline uint8_t rmt_int_idx(E_RMT_CHANNEL eChannel, E_RMT_INT_TYPE eType) {
     return eType == RMT_INT_TXTHRES ?
             24U + (eChannel & 7) :
             3U * (eChannel & 7) + (uint8_t) eType;
   }
 
+  /**
+   * Tells the masking value of an interrupt in RMT interrupt registers.
+   * @param eChannel Identifies the RMT channel.
+   * @param eType Type of the RMT interrupt.
+   * @return Masking value of the given interrupt.
+   */
   static inline uint32_t rmt_int_bit(E_RMT_CHANNEL eChannel, E_RMT_INT_TYPE eType) {
-    return 1 << rmt_int_bit(eChannel, eType);
+    return 1 << rmt_int_idx(eChannel, eType);
   }
 
+  /**
+   * Tells the output signal number of an RMT channel in the GPIO matrix.
+   * @param eChannel Identifies the RMT channel.
+   * @return GPIO output signal number.
+   */
   static inline uint8_t rmt_out_signal(E_RMT_CHANNEL eChannel) {
     return 87 + eChannel;
   }
+
+  /**
+   * Tells the input signal number of an RMT channel in the GPIO matrix.
+   * @param eChannel Identifies the RMT channel.
+   * @return GPIO input signal number.
+   */
+  static inline uint8_t rmt_in_signal(E_RMT_CHANNEL eChannel) {
+    return 83 + eChannel;
+  }
+
+  /**
+   * Start data transmit.
+   * @param eChannel Identifies the channel.
+   * @param bMemRdRst Should the controller reset its memory read address for the channel.
+   */
+  static inline void rmt_start_tx(E_RMT_CHANNEL eChannel, bool bMemRdRst) {
+    S_RMT_CHCONF1_REG rConf1 = {.bTxStart = 1, .bMemRdRst = bMemRdRst};
+    gpsRMT->asChConf[eChannel].r1.raw |= rConf1.raw;
+  }
+
+  /**
+   * Start receiving data.
+   * @param eChannel Identifies the channel.
+   * @param bMemWrRst Should the controller reset its memory write address for the channel.
+   */
+  static inline void rmt_start_rx(E_RMT_CHANNEL eChannel, bool bMemWrRst) {
+    S_RMT_CHCONF1_REG rConf1 = {.bRxEn = 1, .bMemWrRst = bMemWrRst};
+    gpsRMT->asChConf[eChannel].r1.raw |= rConf1.raw;
+  }
+
+#undef RMT_CHANNEL_NUM
+#undef RMT_RAM_BLOCK_SIZE
 
 #ifdef __cplusplus
 }
